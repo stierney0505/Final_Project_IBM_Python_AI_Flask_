def01906d4d142999377ed4f4bc0f22d9ab6a847
stierney0505 <stierney0505@gmail.com>
2023-09-05 20:07:05 -0400

    Began final project

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,46 @@
+import requests
+import json
+
+def sentiment_analyzer(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+    formattedResponse = json.loads(response.text)
+
+    data = formattedResponse['emotionPredictions'][0]['emotion'];
+    emotions = []
+    dominant_emotion = ''
+    dominant_value = -1
+    for key in data:
+        if(key == 'anger'):
+            emotions.append(key)
+            if(data[key] > dominant_value):
+                dominant_emotion = key
+        elif(key == 'disgust'):
+            emotions.append(key)
+            if(data[key] > dominant_value):
+                dominant_emotion = key
+        elif(key == 'fear'):
+            emotions.append(key)
+            if(data[key] > dominant_value):
+                dominant_emotion = key
+        elif(key == 'joy'):
+            emotions.append(key)
+            if(data[key] > dominant_value):
+                dominant_emotion = key
+        elif(key == 'sadness'):
+            emotions.append(key)
+            if(data[key] > dominant_value):
+                dominant_emotion = key
+
+    returnJSON = {emotions[0]: data[emotions[0]],
+                  emotions[1]: data[emotions[1]],
+                  emotions[2]: data[emotions[2]],
+                  emotions[3]: data[emotions[3]],
+                  emotions[4]: data[emotions[4]],
+                  'dominant_emotion' : dominant_emotion
+                 }
+
+
+    return json.dumps(returnJSON)
