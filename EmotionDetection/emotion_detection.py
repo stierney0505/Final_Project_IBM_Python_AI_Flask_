e57fd6285214a2e8db79ef664cf8f51e3ce1c332
stierney0505 <stierney0505@gmail.com>
2023-09-06 14:54:28 -0400

    Finished Project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,6 +8,16 @@ def emotion_analyzer(text_to_analyse):
     response = requests.post(url, json = myobj, headers=header)
     formattedResponse = json.loads(response.text)
 
+    if response.status_code == 400:
+        returnJSON = {'anger': None,
+                      'disgust': None,
+                      'fear': None,
+                      'joy': None,
+                      'sadness': None,
+                      'dominant_emotion' : None
+                     }
+        return json.dumps(returnJSON)
+
     data = formattedResponse['emotionPredictions'][0]['emotion'];
     emotions = []
     dominant_emotion = ''
@@ -39,13 +49,14 @@ def emotion_analyzer(text_to_analyse):
                 dominant_emotion = key
                 dominant_value = data[key]
 
-    returnJSON = {emotions[0]: data[emotions[0]],
-                  emotions[1]: data[emotions[1]],
-                  emotions[2]: data[emotions[2]],
-                  emotions[3]: data[emotions[3]],
-                  emotions[4]: data[emotions[4]],
-                  'dominant_emotion' : dominant_emotion
-                 }
 
+    if response.status_code == 200:
+        returnJSON = {emotions[0]: data[emotions[0]],
+                      emotions[1]: data[emotions[1]],
+                      emotions[2]: data[emotions[2]],
+                      emotions[3]: data[emotions[3]],
+                      emotions[4]: data[emotions[4]],
+                      'dominant_emotion' : dominant_emotion
+                     }
 
     return json.dumps(returnJSON)
--- server.py
+++ server.py
@@ -1,27 +1,45 @@
+"""
+This module creates a server that hosts a basic web page
+that takes text input and analyzes the emotions of the 
+statement and returns the values of the emotions analyzed
+"""
+import json
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_analyzer
-import json
+
+
 
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_detect():
+    """
+    This function sends a request to the api to analyze a string.
+
+    :return: A formatted string with the emotions and scores
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = json.loads(emotion_analyzer(text_to_analyze))
-    returnStr = statement = "For the given statement, the system response is 'anger': {:.6f}, 'disgust': {:.6f}, 'fear': {:.6f}, 'joy': {:.6f} and 'sadness': {:.6f}. The dominant emotion is {}."
-    formatted_returnStr = returnStr.format(
+    if response['anger'] is None:
+        return "Invalid text! Please try again!"
+    return_str = "For the given statement, the system response is 'anger': {:.6f}, " \
+                "'disgust': {:.6f}, 'fear': {:.6f}, 'joy': {:.6f} and 'sadness': {:.6f}." \
+                "The dominant emotion is {}."
+    formatted_return_str = return_str.format(
     response['anger'],
     response['disgust'],
     response['fear'],
     response['joy'],
     response['sadness'],
     response['dominant_emotion'])
-    return(formatted_returnStr)
+    return formatted_return_str
 
 @app.route("/")
 def render_index_page():
+    """
+    route for default page
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
